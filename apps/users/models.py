893c493ba0015ef6986f6cc9b45197b8f70624a5
Shers_um49 <Shers_um49@student.itstep.org>
2025-06-04 18:12:19 +0300

    Added some more pretty styles

--- apps/users/models.py
+++ apps/users/models.py
@@ -7,8 +7,10 @@ class CustomUser(AbstractUser):
     ROLE_CHOICES = [
         ('guest', 'Guest'),
         ('user', 'User'),
+        ('worker', 'Worker'),
         ('admin', 'Admin'),
     ]
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='admin')
+    is_worker = False # Field to know if our User is Specialist, Client by default
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
 
